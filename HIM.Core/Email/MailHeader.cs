f90ff4f4ba1eab67e2d547ec2a07e71de2351c70
Kundan Singh <Kundan.Singh@ocean-holidays.co.uk>
2018-03-19 12:02:18 +0530

    commit

---
+++ HIM.Core/Email/MailHeader.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace HIM.Core
+{
+    public class MailHeader
+    {
+        public string From { get; set; }
+        public string SenderName { get; set; }
+        public List<string> To { get; set; } = new List<string>();
+        public List<string> CC { get; set; } = new List<string>();
+        public List<string> BCC { get; set; } = new List<string>();
+        public string Subject { get; set; }
+        public string ReplyTo { get; set; }
+        public string FromName { get; set; }
+        public bool IsMailBodyManualSupply { get; set; }
+        public bool IsEnquiry { get; set; }
+    }
+}
